6a0c4f2c482d81c54a2cb8377a583125899f557c
DB Server <online@media.ucla.edu>
2013-11-11 18:19:02 -0800

    Initial commit to get on github

---
+++ map.js
@@ -0,0 +1,607 @@
+$(document).ready(function() {
+// begin Ready
+
+	// When a dot is clicked
+	//dots for 2011 map (international homepage)******************************************************************************
+	$('a.dotchina11').click(function(){
+
+		$('a.dotchina11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	$('a.dotsouthkorea11').click(function(){
+
+		$('a.dotsouthkorea11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotindia11').click(function(){
+
+		$('a.dotindia11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	$('a.dotcanada11').click(function(){
+
+		$('a.dotcanada11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+		$('a.dottaiwan11').click(function(){
+
+		$('a.dottaiwan11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotsingapore11').click(function(){
+
+		$('a.dotsingapore11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dothongkong11').click(function(){
+
+		$('a.dothongkong11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotunitedkingdom11').click(function(){
+
+		$('a.dotunitedkingdom11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotindonesia11').click(function(){
+
+		$('a.dotindonesia11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotthailand11').click(function(){
+
+		$('a.dotthailand11').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	//functions for 2010 dots******************************************************************************
+	$('a.dotchina10').click(function(){
+
+		$('a.dotchina10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	$('a.dotsouthkorea10').click(function(){
+
+		$('a.dotsouthkorea10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotindia10').click(function(){
+
+		$('a.dotindia10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	$('a.dotcanada10').click(function(){
+
+		$('a.dotcanada10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+		$('a.dottaiwan10').click(function(){
+
+		$('a.dottaiwan10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotsingapore10').click(function(){
+
+		$('a.dotsingapore10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dothongkong10').click(function(){
+
+		$('a.dothongkong10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotunitedkingdom10').click(function(){
+
+		$('a.dotunitedkingdom10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotindonesia10').click(function(){
+
+		$('a.dotindonesia10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotthailand10').click(function(){
+
+		$('a.dotthailand10').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	//dots for 2009 map******************************************************************************
+	$('a.dotchina09').click(function(){
+
+		$('a.dotchina09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	$('a.dotsouthkorea09').click(function(){
+
+		$('a.dotsouthkorea09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotindia09').click(function(){
+
+		$('a.dotindia09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	$('a.dotcanada09').click(function(){
+
+		$('a.dotcanada09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+		$('a.dottaiwan09').click(function(){
+
+		$('a.dottaiwan09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotsingapore09').click(function(){
+
+		$('a.dotsingapore09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dothongkong09').click(function(){
+
+		$('a.dothongkong09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotunitedkingdom09').click(function(){
+
+		$('a.dotunitedkingdom09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotindonesia09').click(function(){
+
+		$('a.dotindonesia09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotthailand09').click(function(){
+
+		$('a.dotthailand09').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	//dots for 2008 map******************************************************************************
+	$('a.dotchina08').click(function(){
+
+		$('a.dotchina08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	$('a.dotsouthkorea08').click(function(){
+
+		$('a.dotsouthkorea08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotindia08').click(function(){
+
+		$('a.dotindia08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+	$('a.dotcanada08').click(function(){
+
+		$('a.dotcanada08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+		$('a.dottaiwan08').click(function(){
+
+		$('a.dottaiwan08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotsingapore08').click(function(){
+
+		$('a.dotsingapore08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dothongkong08').click(function(){
+
+		$('a.dothongkong08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotunitedkingdom08').click(function(){
+
+		$('a.dotunitedkingdom08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotindonesia08').click(function(){
+
+		$('a.dotindonesia08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+	
+		$('a.dotthailand08').click(function(){
+
+		$('a.dotthailand08').removeClass('selected');
+		$(this).addClass('selected');
+
+		var country = '.countrydetail#' + $(this).attr('country');
+		var htmlCode = $(country).html();
+
+		$('.detailcontainer').fadeOut(500, function(){
+			$('.detailcontainer .countrydetail').html(htmlCode);
+			$('.detailcontainer').fadeIn(500);
+		});
+
+	});
+
+
+// end Ready
+});// JavaScript Document
